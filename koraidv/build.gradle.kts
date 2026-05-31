a76cc3493a8f08e7d92d0b30332500894cdeb2fd
badedokun <developer@korastratum.com>
2026-05-31 13:21:51 -0400

    v1.8.2: lockstep bump (no source change)

--- koraidv/build.gradle.kts
+++ koraidv/build.gradle.kts
@@ -107,7 +107,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.koraidv"
             artifactId = "sdk"
-            version = "1.8.1"
+            version = "1.8.2"
 
             afterEvaluate {
                 from(components["release"])
