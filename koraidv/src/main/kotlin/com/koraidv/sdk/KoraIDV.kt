019e2b65c5a630c4862d05f0272cee3bf943d6d5
badedokun <developer@korastratum.com>
2026-06-28 22:24:09 -0400

    v1.9.6: version parity (liveness oval-gate already shipped; gesture config is session-driven server-side)

--- koraidv/src/main/kotlin/com/koraidv/sdk/KoraIDV.kt
+++ koraidv/src/main/kotlin/com/koraidv/sdk/KoraIDV.kt
@@ -51,7 +51,7 @@ object KoraIDV {
     /**
      * SDK version
      */
-    const val VERSION = "1.9.3"
+    const val VERSION = "1.9.6"
 
     /**
      * Configure the SDK with the provided configuration.
